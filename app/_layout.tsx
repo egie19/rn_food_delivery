2001b6ff69eb4ca2f670ca0ea889415b42b028de
Lowiegie Oblenida <lowiegie@gmail.com>
2025-07-10 13:31:25 +0800

    feat: setup sentry application monitoring

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,25 @@ import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 import "./globals.css";
+import * as Sentry from '@sentry/react-native';
 
-export default function RootLayout() {
+Sentry.init({
+  dsn: 'https://599149bec8339848ed1d08b244dbf466@o4509639362215936.ingest.us.sentry.io/4509639396818944',
+
+  // Adds more context data to events (IP address, cookies, user, etc.)
+  // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
+  sendDefaultPii: true,
+
+  // Configure Session Replay
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+
+  // uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: __DEV__,
+});
+
+export default Sentry.wrap(function RootLayout() {
   const [fontsLoaded, error] = useFonts({
     "QuickSand-Bold": require("../assets/fonts/Quicksand-Bold.ttf"),
     "QuickSand-Medium": require("../assets/fonts/Quicksand-Medium.ttf"),
@@ -18,4 +35,4 @@ export default function RootLayout() {
   }, [fontsLoaded, error]);
 
   return <Stack screenOptions={{ headerShown: false }} />;
-}
+});
